1897f26b6215cea6a794a074805a82a2643361ad
omm prakash das <ommprakashdas04@gmail.com>
2025-11-16 08:35:52 +0530

    homepage react

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,4 +1,8 @@
 import { useState } from 'react'
+import './assets/css/style.css'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
 
 
 
@@ -7,6 +11,9 @@ function App() {
 
   return (
     <>
+      <Header />
+      <Main />
+      <Footer/>
       
     </>
   )
