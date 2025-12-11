f37c516e8b1addd2e61f3ebe77d62c8fe0cc8f5a
omm prakash das <ommprakashdas04@gmail.com>
2025-12-11 08:40:47 +0530

    registration page

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -3,7 +3,9 @@ import './assets/css/style.css'
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
-
+import Register from './components/Register'
+import Login from './components/login'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 
 
 function App() {
@@ -11,9 +13,19 @@ function App() {
 
   return (
     <>
+      <BrowserRouter >
       <Header />
-      <Main />
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/register' element={<Register />}/>
+        <Route path='/login' element = {<Login/>} />
+      </Routes>
       <Footer/>
+      </BrowserRouter>
+  
+
+      
+      
       
     </>
   )
--- frontend-react/src/components/Main.jsx
+++ frontend-react/src/components/Main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
 import Button from './Button'
 
 const Main = () => {
   return (
     <>
+    
       <div className='container'>
         <div className='p-5 text-center bg-light-dark rounded'>
         <h1 className='text-light'>Stock Prediction Portal</h1>
